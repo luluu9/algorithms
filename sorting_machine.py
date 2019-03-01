bf2b1ea818a638a62d45a15fe7af07adc73d99eb
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2019-03-02 00:23:15 +0100

    Add merge sorting

--- sorting_machine.py
+++ sorting_machine.py
@@ -4,7 +4,7 @@ from random import randint
 def generate_lst(length):
     lst = []
     for i in range(length):
-        lst.append(randint(0, 100000))
+        lst.append(randint(0, 1000))
     return lst
 
 
@@ -12,7 +12,7 @@ def bubble_sorting(lst):
     moved = True
     while moved is True:
         moved = False
-        for i in range(len(lst) - 1):
+        for i in range(len(lst)-1):
             if lst[i] > lst[i + 1]:
                 temp = lst[i]
                 lst[i] = lst[i + 1]
@@ -21,5 +21,53 @@ def bubble_sorting(lst):
     return lst
 
 
-lst = generate_lst(10)
+def selection_sorting(lst):
+    d = len(lst)
+    for i in range(d):
+        smallest = lst[i]
+        smallestIndex = i
+        for j in range(i + 1, d):
+            if (lst[j] < smallest):
+                smallest = lst[j]
+                smallestIndex = j
+        buff = lst[i]
+        lst[i] = smallest
+        lst[smallestIndex] = buff
+    return lst
+
+
+def merge_sorting(lst):
+    def merge(left_lst, right_lst):
+        lst = []
+        final_len = len(left_lst) + len(right_lst)
+        while(len(lst) < final_len):
+            if not left_lst:
+                lst += right_lst
+                break
+            if not right_lst:
+                lst += left_lst
+                break
+            #########
+            if left_lst[0] < right_lst[0]:
+                lst.append(left_lst.pop(0))
+            elif right_lst[0] < left_lst[0]:
+                lst.append(right_lst.pop(0))
+            else:
+                lst.append(left_lst.pop(0))
+        return lst
+
+    if len(lst) < 2:
+        return lst
+    else:
+        left_lst = lst[:len(lst) // 2]
+        right_lst = lst[len(lst) // 2:]
+        left_lst_sorted = merge_sorting(left_lst)
+        right_lst_sorted = merge_sorting(right_lst)
+        lst = merge(left_lst_sorted, right_lst_sorted)
+        return lst
+
+
+lst = generate_lst(30)
+print(selection_sorting(lst))
 print(bubble_sorting(lst))
+print(merge_sorting(lst))
